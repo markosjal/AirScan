11e76a3193f5999d02536a7f4b15503a9c00c5f1
markosjal <markosjal@gmail.com>
2020-02-10 07:46:39 -0600

    Add files via upload

--- scans/Test/index.php
+++ scans/Test/index.php
@@ -1,26 +1,29 @@
 <?php
-include_once '../../phppagestart.php';
+include_once '../phppagestart.php';
 //error_reporting( -1 );
 //ini_set( 'display_errors', 1 );
+// session_start();
+// echo $_SESSION['username'];
+
+
 $username= $_SESSION['username'];
 
 // The following two lines MUST point to <WEBROOT>/config.inc.php 
 // first how we get back to webroot from ths user../
-$webroot='/var/www/html/';
+$webroot='../';
 include_once($webroot.'config.inc.php');
 include_once($webroot.'lang.php');
-chdir ($webroot);
+chdir ($root);
 $_SESSION['fromuserfolder']='yes';
-$_SESSION['fromuserfilelister']='yes';
+// $_SESSION['fromuserfilelister']='yes';
 $now = time();
 
+/*
 if (($requireauth == 'yes') && ($_SESSION['loggedin'] != 'yes'))
 {
-
-//echo 'excuse 1';
 include_once('deauthorize.php');
 }
-
+*/
 if (($requireauth == 'yes') && ($_SESSION['loggedin'] == 'yes') && ($_SESSION['expire'] > $now))//  && ($_SESSION['expire'] > $now))
 {
 include_once('filelister.php');
@@ -30,17 +33,17 @@ elseif ($requireauth != 'yes')
 {
 include_once('filelister.php');
 }
-
+/*
 elseif (($requireauth == 'yes') && ($_SESSION['loggedin'] != 'yes'))
 {
-//echo 'excuse 2';
 include_once('deauthorize.php');
 }
 
 elseif (($requireauth == 'yes') && ($_SESSION['expire'] <= $now))
 {
-//echo 'excuse 3';
 include_once('deauthorize.php');
 }
+*/
 
 ?>
+
