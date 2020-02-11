601bb2e0190ecfd01ee64221d56ba863bc8eb1e1
markosjal <markosjal@gmail.com>
2020-02-11 09:29:14 -0600

    Update index.php
    Cleanup

--- eSCL/Scans/index.php
+++ eSCL/Scans/index.php
@@ -1,20 +1,15 @@
 <?php 
-include $webroot.'/confg.inc.php';
-if ($_SERVER['REQUEST_METHOD']== 'DELETE')
+//error_reporting( -1 );
+//ini_set( 'display_errors', 1 );
+include_once('/var/www/html/config.inc.php');
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
 {
-	if ($saveallesclfiles != 'yes')
-	{
-	unlink ('XYZ.jpg');
-	}
+        if ($saveallesclfiles != 'yes')
+        {
+        unlink ('XYZ.jpg');
+        }
 }
-
-//$delete= file_get_contents('php://input');
-
-
-//$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
-
-//xecho $delete;
-
 
 
 ?>
+
