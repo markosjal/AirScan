154a278328367429ed4a7b302bcf5be7bf07c1f7
markosjal <markosjal@gmail.com>
2020-02-11 09:33:17 -0600

    Update index.php

--- eSCL/Scans/index.php
+++ eSCL/Scans/index.php
@@ -1,7 +1,7 @@
 <?php 
 //error_reporting( -1 );
 //ini_set( 'display_errors', 1 );
-include_once('/var/www/html/config.inc.php');
+include_once('../../config.inc.php');
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
 {
         if ($saveallesclfiles != 'yes')
