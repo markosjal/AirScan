c1602a30d9e431dd34f2a77c631628223af7628a
markosjal <markosjal@gmail.com>
2020-02-17 09:57:12 -0600

    Update index.php

--- eSCL/ScanJobs/index.php
+++ eSCL/ScanJobs/index.php
@@ -1,6 +1,6 @@
 <?php
 header('HTTP/1.1 201 Created');
-header('Location: http://'.gethostname().'/eSCL/Scans/');
+header('Location: http://'.gethostname().'.local/eSCL/Scans/');
 header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' ); 
 header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' ); 
 header( 'Cache-Control: no-store, no-cache, must-revalidate' ); 
